b18dc42d524cd196885818b27b98d1c294d6a505
Gianluca Cabrera <gianlucacabrera@gmail.com>
2026-03-13 15:47:00 +0100

    Collapse classes and change nb to those 3 classes

--- extraction.py
+++ extraction.py
@@ -4,13 +4,18 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report, f1_score
 import pandas as pd
+import re
 
 
 ds = load_dataset("SemEvalWorkshop/sem_eval_2010_task_8")
 nb = MultinomialNB(alpha=0.25)
 
-for i in range(2):
-    print(ds["train"][i])
+def _collapse_relations(batch):
+    def map_rel(r):
+        return 0 if r == 0 else (1 if r ==  1 else 2)
+    return {"relation": [map_rel(r) for r in batch["relation"]]}
+
+ds = ds.map(_collapse_relations, batched=True)
 
 vectorizer = CountVectorizer(
     max_features=10000, 
@@ -26,6 +31,8 @@ y_test = ds["test"]["relation"]
 nb.fit(X_train_vec, y_train)
 
 y_pred = nb.predict(X_test_vec)
+
+# Evaluation
 
 print(classification_report(y_test, y_pred))
 
