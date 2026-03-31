e15663196ad5ca30b69e2873c3129da396230657
Gianluca Cabrera <gianlucacabrera@gmail.com>
2026-03-31 09:31:02 +0200

    Change preprocessing for each use case and add lowecasing

--- src/data/data.py
+++ src/data/data.py
@@ -1,15 +1,23 @@
 from datasets import load_dataset
 
-def load_and_process(dataset_name):
+def load_and_process(dataset_name, variant):
     dataset = load_dataset(dataset_name)
-
 
     def _collapse_relations(batch):
         def map_rel(r):
             return 0 if r == 0 else (1 if r ==  1 else 2)
         return {"relation": [map_rel(r) for r in batch["relation"]]}
+    
+    def _to_lower(batch):
+       return {"sentence": [s.lower() for s in batch["sentence"]]}
 
-    dataset = dataset.map(_collapse_relations, batched=True)
+    if variant == "nb_collapsed":
+        dataset = dataset.map(_to_lower, batched=True)
+        dataset = dataset.map(_collapse_relations, batched=True)
+    elif variant == "nb_full":
+        dataset = dataset.map(_to_lower, batched=True)
+    elif variant == "roberta_collapsed":
+        dataset = dataset.map(_collapse_relations, batched=True)
     dataset = dataset.rename_column("relation", "labels")
 
     return dataset
--- src/models/baseline.py
+++ src/models/baseline.py
@@ -3,20 +3,21 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import classification_report, f1_score
 from src.data.data import load_and_process
 
-semeval = load_and_process("SemEvalWorkshop/sem_eval_2010_task_8")
+semeval_full = load_and_process("SemEvalWorkshop/sem_eval_2010_task_8", "nb_full")
+semeval_collapsed = load_and_process("SemEvalWorkshop/sem_eval_2010_task_8", "nb_collapsed")
 
-nb = MultinomialNB(alpha=0.25)
+nb = MultinomialNB(alpha=1)
 
 vectorizer = CountVectorizer(
     max_features=10000, 
-    ngram_range=(1, 2), 
+    ngram_range=(1, 1),
 )
 
-X_train_vec = vectorizer.fit_transform(semeval["train"]["sentence"])
-X_test_vec = vectorizer.transform(semeval["test"]["sentence"])
+X_train_vec = vectorizer.fit_transform(semeval_full["train"]["sentence"])
+X_test_vec = vectorizer.transform(semeval_full["test"]["sentence"])
 
-y_train = semeval["train"]["relation"]
-y_test = semeval["test"]["relation"]
+y_train = semeval_full["train"]["labels"]
+y_test = semeval_full["test"]["labels"]
 
 nb.fit(X_train_vec, y_train)
 
